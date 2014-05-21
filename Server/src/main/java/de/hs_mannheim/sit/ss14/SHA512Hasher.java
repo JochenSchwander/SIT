bee50e4be72b0928f4ea8f993e7b02f2d1b83978
Phil Kwiotek <info@kwiotec.de>
2014-05-22 00:32:28 +0200

    Hasher angefangen zu implementieren

--- Server/src/main/java/de/hs_mannheim/sit/ss14/SHA512Hasher.java
+++ Server/src/main/java/de/hs_mannheim/sit/ss14/SHA512Hasher.java
@@ -1,8 +1,12 @@
 package de.hs_mannheim.sit.ss14;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
 
 /**
  *
@@ -23,5 +27,27 @@ public class SHA512Hasher implements Hasher{
 	      }
 	      return input;
 	}
+
+	  /**
+	   * From a base 64 representation, returns the corresponding byte[]
+	   * @param data String The base64 representation
+	   * @return byte[]
+	   * @throws IOException
+	   */
+	  public static byte[] base64ToByte(String data) throws IOException {
+	      BASE64Decoder decoder = new BASE64Decoder();
+	      return decoder.decodeBuffer(data);
+	  }
+
+	  /**
+	   * From a byte[] returns a base 64 representation
+	   * @param data byte[]
+	   * @return String
+	   * @throws IOException
+	   */
+	  public static String byteToBase64(byte[] data){
+	      BASE64Encoder endecoder = new BASE64Encoder();
+	      return endecoder.encode(data);
+	  }
 
 }
