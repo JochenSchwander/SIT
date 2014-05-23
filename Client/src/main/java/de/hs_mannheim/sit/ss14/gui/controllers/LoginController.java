5de23a32d13d92d8734c48a3c61623c7a96638c7
trippledave <davedannstadt@abwesend.de>
2014-05-23 11:42:15 +0200

    updated view further

--- Client/src/main/java/de/hs_mannheim/sit/ss14/gui/controllers/LoginController.java
+++ Client/src/main/java/de/hs_mannheim/sit/ss14/gui/controllers/LoginController.java
@@ -6,6 +6,12 @@ import java.awt.event.ActionListener;
 import de.hs_mannheim.sit.ss14.gui.models.LoginModel;
 
 public class LoginController {
+	
+	private GuiController guiController;
+	LoginController(GuiController guiController){
+		this.guiController=guiController;
+		
+	}
 
 	/**
 	 * ActionListener for when the Login button gets pressed.
@@ -17,13 +23,33 @@ public class LoginController {
 		return new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				loginModel.credentialsMessageTextarea.setText("Username: "
-						+ loginModel.usernameTextfield.getText()
-						+ "\nPasswort: "
-						+ loginModel.passwordTextfield.getText());
-
+				startLoginProcess(loginModel);
 			}
 		};
+	}
+
+	public ActionListener getPasswordTextfieldAL(final LoginModel loginModel) {
+		return new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				startLoginProcess(loginModel);
+			}
+		};
+	}
+
+	private void startLoginProcess(LoginModel loginModel) {
+		//guiController.setLoggedInView();
+		boolean test=true;
+		while(test){
+			System.out.println("");
+		}
+		loginModel.credentialsMessageTextarea.setText("Username: "
+				+ loginModel.usernameTextfield.getText() + "\nPasswort: "
+				+ loginModel.passwordTextfield.getText());
+		
+		
+		
+		
 	}
 
 }
