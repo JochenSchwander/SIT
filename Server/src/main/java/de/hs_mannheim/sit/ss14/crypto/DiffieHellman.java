3f161353b742c26de7ac21248ad918f96dc21bcd
trippledave <davedannstadt@abwesend.de>
2014-05-25 13:31:17 +0200

    added Oracle copyright

--- Server/src/main/java/de/hs_mannheim/sit/ss14/crypto/DiffieHellman.java
+++ Server/src/main/java/de/hs_mannheim/sit/ss14/crypto/DiffieHellman.java
@@ -1,5 +1,6 @@
 package de.hs_mannheim.sit.ss14.crypto;
 
+import java.math.BigInteger;
 import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
@@ -11,9 +12,40 @@ import javax.crypto.interfaces.DHPublicKey;
 import javax.crypto.spec.DHParameterSpec;
 
 /**
- * Diffie Hellman Algorithm implementation.
+ * Diffie Hellman Algorithm implementation by Oracle.
+ * Split up in Server and Client Classes by DS.
+ * Function names and comments by Jochen Schwander.
+ * 
+ * @src http://docs.oracle.com/javase/7/docs/technotes/guides/security/crypto/CryptoSpec.html Appendix D
+ * 
+ * Copyright (c) 1997, 2001, Oracle and/or its affiliates. All rights reserved.
  *
- * @author DS
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Oracle nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 public class DiffieHellman {
 	private KeyAgreement keyAgree;
@@ -104,15 +136,10 @@ public class DiffieHellman {
 		return buf.toString();
 	}
 
-	/*
+	/**
 	 * Converts a hex string  to byte array
 	 */
-	private byte[] hexStringToByteArray(String s) {
-	    int len = s.length();
-	    byte[] data = new byte[len / 2];
-	    for (int i = 0; i < len; i += 2) {
-	        data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i+1), 16));
-	    }
-	    return data;
+	private static byte[] hexStringToByteArray(String hexString) {
+		return (new BigInteger(hexString, 16)).toByteArray();
 	}
 }
